8650d98cb091f59937bed32c1115a6237ca06a70
Charlotte Fung <fungcha@shernet.sheridancollege.ca>
2024-03-13 15:56:49 -0400

    working on the dash

---
+++ Assets/Week 9/Scripts/Thief.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thief : Villager
+{
+
+    public GameObject knifePrefab;
+    public GameObject knifePrefab2;
+    public Transform spawnpoint;
+    public Transform spawnpoint2;
+    public float dashSpeed = 5f;
+
+    protected override void Attack()
+    {
+        base.Attack();
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = (mousePosition - transform.position).normalized;
+        Instantiate(knifePrefab, spawnpoint.position, spawnpoint.rotation);
+        Instantiate(knifePrefab2,spawnpoint2.position, spawnpoint2.rotation);
+    }
+
+    public override ChestType CanOpen()
+    {
+        return ChestType.Thief;
+    }
+}
