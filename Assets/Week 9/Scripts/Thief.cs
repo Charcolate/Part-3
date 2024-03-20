066fa675ef68dd8f810846cd7e254416d36b7185
Charlotte Fung <fungcha@shernet.sheridancollege.ca>
2024-03-20 14:00:19 -0400

    complete task 1

--- Assets/Week 9/Scripts/Thief.cs
+++ Assets/Week 9/Scripts/Thief.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class Thief : Villager
@@ -9,21 +10,41 @@ public class Thief : Villager
     public GameObject knifePrefab2;
     public Transform spawnpoint;
     public Transform spawnpoint2;
-    public float dashSpeed = 5f;
+    public float dashspeed = 7f;
+    //float timer;
+    //public float dashtime = 2;
+    //bool isDashing;
 
     protected override void Attack()
     {
+        StartCoroutine(Dash());
+    }
+
+    IEnumerator Dash()
+    {
+        destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        speed = 7;
+
+        while (speed > 3)
+        {
+            yield return null;
+        }
+
         base.Attack();
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 direction = (mousePosition - transform.position).normalized;
-        Vector2 moveTowards = new Vector2(direction.x, direction.y) * dashSpeed * Time.deltaTime;
-        destination = mousePosition;
+        yield return new WaitForSeconds(0.1f);
         Instantiate(knifePrefab, spawnpoint.position, spawnpoint.rotation);
-        Instantiate(knifePrefab2,spawnpoint2.position, spawnpoint2.rotation);
+        yield return new WaitForSeconds(0.1f);
+        Instantiate(knifePrefab2, spawnpoint2.position, spawnpoint2.rotation);
+       
     }
 
     public override ChestType CanOpen()
     {
         return ChestType.Thief;
+    }
+
+    public override string ToString()
+    {
+        return "Hi, I'm Bob!";
     }
 }
