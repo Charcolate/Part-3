af46af48810c1b842fa9cd172350583cda5261c4
Charlotte Fung <fungcha@shernet.sheridancollege.ca>
2024-03-31 17:54:44 -0400

    complete a static function for checcking the players are "eating the food"

--- Assets/Assignment/scripts/food.cs
+++ Assets/Assignment/scripts/food.cs
@@ -6,6 +6,12 @@ public class food : MonoBehaviour
 {
     public static bool isEating = false; //static variable to check if the players are eating the food
 
+    public static void StartEating() //static function
+    {
+        isEating = true;
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
